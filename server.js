252ef33ab6be47fce53cb5a9400a790282ca93fd
youssefelghamour <youssefelghamour@gmail.com>
2024-08-03 04:25:52 -0700

    2. First API

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes/index';
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default app;
