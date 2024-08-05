0c448a5b052ab89a5985d6ed571e2991201c913a
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-08-05 20:17:55 +0000

    6. Get and list file

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -102,7 +102,7 @@ class FilesController {
     }
 
     const { id } = request.params;
-    const file = await dbClient.filesCollection.findOne({ _id: new ObjectId(id), userId: userId });
+    const file = await dbClient.filesCollection.findOne({ _id: new ObjectId(id) });
     if (!file) {
       return response.status(404).json({ error: 'Not found' });
     }
